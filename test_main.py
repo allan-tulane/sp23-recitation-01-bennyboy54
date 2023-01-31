d7b3a00fa61a80425dd4efdff1cf027810076d27
bennyboy54 <benhwr@live.com>
2023-01-31 15:54:57 -0600

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -12,10 +12,9 @@ def test_binary_search():
 	assert binary_search([1,2,3,4,5], 5) == 4
 	assert binary_search([1,2,3,4,5], 1) == 0
 	assert binary_search([1,2,3,4,5], 6) == -1
-	### TODO: add two more tests here.
-  assert binary_search([1,2,3,4,5,6], 1) == 0
-  assert binary_search([1,2,3,4,5], 3) == 2
-	###
+	
+	assert binary_search([1,2,3,4,5,6], 1) == 0
+	assert binary_search([1,2,3,4,5], 3) == 2
 
 def test_linear_search():
 	""" done. """
